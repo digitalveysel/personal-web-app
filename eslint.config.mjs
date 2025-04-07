b9e08cfa7263f46e2ba4a0c4ca8989f855b6d6b9
digitalveysel <hello@veysel.co>
2025-04-07 19:59:31 +0300

    update eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
 import prettierPlugin from 'eslint-plugin-prettier';
-import prettierConfig from './.prettierrc.cjs';
+import prettierConfig from './prettier.config.mjs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
