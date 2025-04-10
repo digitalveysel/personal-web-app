cb44ed6ac61eb471a26dde777d74bb5dc96a6d32
digitalveysel <hello@veysel.co>
2025-04-10 03:02:13 +0300

    update loader cols component

--- src/components/features/LoaderCols.tsx
+++ src/components/features/LoaderCols.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import clsx from 'clsx';
 import { motion } from 'motion/react';
 
 export default function LoaderColumns() {
@@ -7,34 +8,41 @@ export default function LoaderColumns() {
 
   return (
     <motion.div
-      className="pointer-events-none fixed inset-0 h-screen w-screen"
+      className="pointer-events-none fixed inset-0 flex"
       initial={{ zIndex: '9' }}
       animate={{ zIndex: '-1' }}
-      transition={{ duration: 1, delay: 2, ease: 'linear' }}
-      aria-hidden="true"
+      transition={{ duration: 1, delay: 1, ease: 'linear' }}
+      aria-hidden
     >
       {columns.map((_, i) => (
-        <div key={i} className="relative inline-block h-screen w-1/5 align-top">
+        <div
+          key={i}
+          className={clsx('relative basis-full align-top', {
+            'hidden md:block': i === 2,
+            'hidden lg:block': i === 3,
+            'hidden xl:block': i === 4,
+          })}
+        >
           {i !== 0 && (
             <motion.span
               className="bg-background-transparent absolute top-0 left-0 w-px"
               initial={{ height: 0 }}
               animate={{ height: '100%' }}
               transition={{
-                duration: 1,
+                duration: 0.4,
                 ease: 'easeOut',
-                delay: i % 2 === 0 ? 0.8 : 0.4,
+                delay: i * 0.2,
               }}
             />
           )}
           <motion.span
-            className="bg-background-default absolute top-0 left-0 h-full w-full"
+            className={`bg-background-medium absolute ${i % 2 === 0 ? 'top-0' : 'bottom-0'} left-0 size-full`}
             initial={{ height: '100%' }}
             animate={{ height: 0 }}
             transition={{
-              duration: 1,
+              duration: 0.4,
               ease: 'easeOut',
-              delay: i % 2 === 0 ? 0.4 : 0.8,
+              delay: i * 0.2,
             }}
           />
         </div>
